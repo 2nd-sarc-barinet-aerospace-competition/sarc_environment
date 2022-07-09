22e7e9e67812aa0b4385a3267fbb7f714b4104c5
JorgelHenri <jorge.luiz.henri@hotmail.com>
2022-07-09 02:54:47 -0300

    added air resistence

--- scripts/fall.py
+++ scripts/fall.py
@@ -7,14 +7,19 @@ import math
 from mrs_msgs.msg import UavManagerDiagnostics as UavManagerDiagnostics
 from mavros_msgs.srv import CommandBool as CommandBool
 from std_srvs.srv import Trigger as Trigger
-from gazebo_msgs.srv import DeleteModel, DeleteModelRequest
+from gazebo_msgs.srv import DeleteModel, DeleteModelRequest, ApplyBodyWrench
+from geometry_msgs.msg import Point, Wrench, Vector3
 
-
-
+global uav_name
+uav_name = "uav1"
 class Activator:
-    
     def callback(self, data):
-
+        body_name = uav_name + '::base_link'
+        wrench = Wrench()
+        force = [2, 2, 2]
+        wrench.force = Vector3(*force)
+        duration = rospy.Duration(40)
+        
         dele = DeleteModelRequest()
         dele.model_name = "SARckc_floor"
         self.delete(dele)
@@ -22,18 +27,21 @@ class Activator:
         self.arm(1)
         rospy.loginfo('activating')
         self.activate()
+        rospy.sleep(1)
+        rospy.loginfo('applying')
+        self.apply_wrench(body_name, 'world', Point(0, 0, 0), wrench, rospy.Time().now(), duration)
         rospy.signal_shutdown("yes")
 
     def __init__(self):
 
-        rospy.init_node('activator', anonymous=True)
+        rospy.init_node('activator', anonymous=True) 
 
-        uav_name = "uav1"
-        self.delete = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)
+        self.delete = rospy.ServiceProxy('/gazebo/delete_model', DeleteModel)    
         self.subscriber = rospy.Subscriber('/' + uav_name + '/uav_manager/diagnostics', UavManagerDiagnostics, self.callback)
         self.arm = rospy.ServiceProxy('/' + uav_name + '/mavros/cmd/arming', CommandBool)
         self.activate = rospy.ServiceProxy('/' + uav_name + '/uav_manager/midair_activation', Trigger)
-
+        self.apply_wrench = rospy.ServiceProxy('/gazebo/apply_body_wrench', ApplyBodyWrench)
+        
         rospy.loginfo('initialized')
         rospy.spin()
 
--- scripts/sarc_low_map.py
+++ scripts/sarc_low_map.py
@@ -18,7 +18,7 @@ rospack = rospkg.RosPack()
 rospack.list()
 rospy.init_node('sarc_low_map', anonymous=True)
 rate = rospy.Rate(10) # 10hz
-quant = 100 ## this quantity is this number times 3, since each tree type will have this many replicas
+quant = 1 ## this quantity is this number times 3, since each tree type will have this many replicas
 dronesquant = 5 ## number of drones to spawn, above 5 pay atention in the spawn circle radius
 spawnCircleRadius = 0.75
 
